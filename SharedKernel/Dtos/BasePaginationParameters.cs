2d1509488b0fd83a39e91779c911a32ddd8c90dd
Paul DeVito <pdevito3@gmail.com>
2023-08-26 22:48:29 -0400

    fix: larger default page size

--- SharedKernel/Dtos/BasePaginationParameters.cs
+++ SharedKernel/Dtos/BasePaginationParameters.cs
@@ -2,7 +2,7 @@ namespace SharedKernel.Dtos
 {
     public abstract class BasePaginationParameters
     {
-        internal virtual int MaxPageSize { get; } = 20;
+        internal virtual int MaxPageSize { get; } = 500;
         internal virtual int DefaultPageSize { get; set; } = 10;
 
         public virtual int PageNumber { get; set; } = 1;
