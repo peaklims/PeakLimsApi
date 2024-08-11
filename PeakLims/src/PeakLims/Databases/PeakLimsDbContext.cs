8149c9c397c5e4d7b6de995792367137a19727b8
Paul DeVito <pdevito3@gmail.com>
2024-08-10 20:36:14 -0400

    refactor: extension methods

--- PeakLims/src/PeakLims/Databases/PeakLimsDbContext.cs
+++ PeakLims/src/PeakLims/Databases/PeakLimsDbContext.cs
@@ -26,6 +26,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Exceptions;
 using Microsoft.EntityFrameworkCore.Query;
 using Resources;
 
@@ -181,5 +182,47 @@ public static class Extensions
             // set filter
             mutableEntityType.SetQueryFilter(lambdaExpression);
         }
+    }
+    
+    public static async Task<TEntity> GetByIdOrDefault<TEntity> (this DbSet<TEntity> dbSet, 
+        Guid id, 
+        CancellationToken cancellationToken = default) 
+            where TEntity : BaseEntity
+    {
+        return await dbSet.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+    }
+    
+    public static async Task<TEntity> GetByIdOrDefault<TEntity> (this IQueryable<TEntity> query, 
+        Guid id, 
+        CancellationToken cancellationToken = default) 
+            where TEntity : BaseEntity
+    {
+        return await query.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+    } 
+    
+    public static async Task<TEntity> GetById<TEntity> (this DbSet<TEntity> dbSet, 
+        Guid id, 
+        CancellationToken cancellationToken = default) 
+            where TEntity : BaseEntity
+    {
+        var result = await dbSet.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+
+        return result.MustBeFoundOrThrow();
+    }
+    
+    public static async Task<TEntity> GetById<TEntity> (this IQueryable<TEntity> query, 
+        Guid id, 
+        CancellationToken cancellationToken = default) 
+            where TEntity : BaseEntity
+    {
+        var result = await query.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+
+        return result.MustBeFoundOrThrow();
+    }
+    
+    public static TEntity MustBeFoundOrThrow<TEntity>(this TEntity entity)
+        where TEntity : BaseEntity
+    {
+         return entity ?? throw new NotFoundException($"{typeof(TEntity).Name} was not found.");
     }
 }
