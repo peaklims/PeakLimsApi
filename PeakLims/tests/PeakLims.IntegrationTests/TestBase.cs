f311ebf140f3903dbd85a676aaada9d0f865a344
Paul DeVito <pdevito3@gmail.com>
2024-10-13 18:12:01 -0400

    feat: auto bogus conventions

--- PeakLims/tests/PeakLims.IntegrationTests/TestBase.cs
+++ PeakLims/tests/PeakLims.IntegrationTests/TestBase.cs
@@ -1,7 +1,7 @@
 namespace PeakLims.IntegrationTests;
 
 using AutoBogus;
-
+using AutoBogus.Conventions;
 
 [Collection(nameof(TestFixture))]
 public class TestBase : IDisposable
@@ -12,6 +12,10 @@ public class TestBase : IDisposable
         {
             // configure global autobogus settings here
             builder.WithDateTimeKind(DateTimeKind.Utc)
+                .WithConventions(config =>
+                {
+                    // config.Email.Aliases("AnotherEmail");  // Generates an email value for members named AnotherEmail
+                })
                 .WithRecursiveDepth(3)
                 .WithTreeDepth(1)
                 .WithRepeatCount(1);
