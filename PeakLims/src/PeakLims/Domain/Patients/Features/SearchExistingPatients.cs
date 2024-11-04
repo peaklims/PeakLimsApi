2bf7a88afd55e0c043646ae249c1c762413ae3ac
Paul DeVito <pdevito3@gmail.com>
2024-11-03 23:45:42 -0500

    fix: patient search

--- PeakLims/src/PeakLims/Domain/Patients/Features/SearchExistingPatients.cs
+++ PeakLims/src/PeakLims/Domain/Patients/Features/SearchExistingPatients.cs
@@ -1,5 +1,6 @@
 namespace PeakLims.Domain.Patients.Features;
 
+using Databases;
 using Exceptions;
 using PeakLims.Domain.Patients.Dtos;
 using PeakLims.Domain.Patients.Services;
@@ -17,17 +18,29 @@ public static class SearchExistingPatients
 {
     public sealed record Query(PatientParametersDto QueryParameters) : IRequest<PagedList<PatientSearchResultDto>>;
 
-    public sealed class Handler(IPatientRepository patientRepository)
+    public sealed class Handler(PeakLimsDbContext dbContext)
         : IRequestHandler<Query, PagedList<PatientSearchResultDto>>
     {
         public async Task<PagedList<PatientSearchResultDto>> Handle(Query request, CancellationToken cancellationToken)
         {
+            var collection = dbContext.Patients
+                .Include(x => x.Accessions)
+                .AsNoTracking();
             
             // var queryKitConfig = new CustomQueryKitConfiguration();
             var queryKitConfig = new CustomQueryKitConfiguration(config =>
             {
                 // config.Property<Patient>(x => x.Accessions).HasQueryName("status");
-                // config.Property<Patient>(x => x.Accessions.Select(y => y.AccessionNumber)).HasQueryName("accessionNumber");
+                config.Property<Patient>(x => x.Accessions.Select(y => y.AccessionNumber))
+                    .HasQueryName("accessionNumber");
+                config.Property<Patient>(x => x.FirstName)
+                    .HasQueryName("firstName");
+                config.Property<Patient>(x => x.LastName)
+                    .HasQueryName("lastName");
+                config.Property<Patient>(x => x.InternalId)
+                    .HasQueryName("internalId");
+                config.Property<Patient>(x => x.Lifespan.DateOfBirth)
+                    .HasQueryName("dob");
             });
             var queryKitData = new QueryKitData()
             {
@@ -35,10 +48,6 @@ public static class SearchExistingPatients
                 SortOrder = request.QueryParameters.SortOrder ?? "-CreatedOn",
                 Configuration = queryKitConfig
             };
-
-            var collection = patientRepository.Query()
-                .Include(x => x.Accessions)
-                .AsNoTracking();
             var appliedCollection = collection.ApplyQueryKit(queryKitData);
             var dtoCollection = appliedCollection.ToPatientSearchResultDtoQueryable();
 
