df177c1a9e1a2418717ac1dd0b20f9fd3cffa7c4
Paul DeVito <pdevito3@gmail.com>
2025-01-25 22:07:43 -0500

    fix: tests

--- PeakLims/tests/PeakLims.SharedTestHelpers/Fakes/User/FakeUserForCreationDto.cs
+++ PeakLims/tests/PeakLims.SharedTestHelpers/Fakes/User/FakeUserForCreationDto.cs
@@ -5,11 +5,13 @@ using PeakLims.Domain;
 using PeakLims.Domain.Users.Dtos;
 using PeakLims.Domain.Roles;
 using PeakLims.Domain.Users.Models;
+using Utilities;
 
 public sealed class FakeUserForCreationDto : AutoFaker<UserForCreationDto>
 {
     public FakeUserForCreationDto()
     {
         RuleFor(u => u.Email, f => f.Person.Email);
+        RuleFor(x => x.OrganizationId, TestingConsts.DefaultTestingOrganizationId);
     }
 }
