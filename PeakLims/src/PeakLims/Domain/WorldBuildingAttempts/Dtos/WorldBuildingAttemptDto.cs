81cdf84b64497d1eb40ff84034538b1e99c47849
Paul DeVito <pdevito3@gmail.com>
2025-01-28 23:36:27 -0500

    feat: better phase info

--- PeakLims/src/PeakLims/Domain/WorldBuildingAttempts/Dtos/WorldBuildingAttemptDto.cs
+++ PeakLims/src/PeakLims/Domain/WorldBuildingAttempts/Dtos/WorldBuildingAttemptDto.cs
@@ -1,10 +1,12 @@
 namespace PeakLims.Domain.WorldBuildingAttempts.Dtos;
 
 using Destructurama.Attributed;
+using WorldBuildingPhases.Dtos;
 
 public sealed record WorldBuildingAttemptDto
 {
     public Guid Id { get; set; }
     public string Status { get; set; }
+    public List<WorldBuildingPhaseDto> Phases { get; set; }
 
 }
--- PeakLims/src/PeakLims/Domain/WorldBuildingAttempts/Mappings/WorldBuildingAttemptMapper.cs
+++ PeakLims/src/PeakLims/Domain/WorldBuildingAttempts/Mappings/WorldBuildingAttemptMapper.cs
@@ -2,10 +2,21 @@ namespace PeakLims.Domain.WorldBuildingAttempts.Mappings;
 
 using PeakLims.Domain.WorldBuildingAttempts.Dtos;
 using Riok.Mapperly.Abstractions;
+using WorldBuildingPhases.Mappings;
 
 [Mapper]
 public static partial class WorldBuildingAttemptMapper
 {
-    public static partial WorldBuildingAttemptDto ToWorldBuildingAttemptDto(this WorldBuildingAttempt worldBuildingAttempt);
+    [MapperIgnoreTarget(nameof(WorldBuildingAttemptDto.Phases))]
+    private static partial WorldBuildingAttemptDto ToWorldBuildingAttemptDtoBase(this WorldBuildingAttempt worldBuildingAttempt);
+
+    public static WorldBuildingAttemptDto ToWorldBuildingAttemptDto(this WorldBuildingAttempt worldBuildingAttempt)
+    {
+        return worldBuildingAttempt.ToWorldBuildingAttemptDtoBase() with
+        {
+            Phases = worldBuildingAttempt.WorldBuildingPhases.Select(x => x.ToWorldBuildingPhaseDto()).ToList()
+        };
+    }  
+    
     public static partial IQueryable<WorldBuildingAttemptDto> ToWorldBuildingAttemptDtoQueryable(this IQueryable<WorldBuildingAttempt> worldBuildingAttempt);
 }
